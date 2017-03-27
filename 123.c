a22985353058917fd27969835b9e6fee41ee6ba0
BoomerDelonge <fiodorste@gmail.com>
2017-03-27 17:02:01 -0500

    Update 123.c

--- 123.c
+++ 123.c
@@ -17,3 +17,34 @@ void main(void){
 		}
 	printf("\n");
 	}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+int m, n;
+char op;
+void main(void){
+
+	printf("Ingrese los números a operar:");
+	scanf("%d %d",&m,&n);
+	getchar();
+	printf("Ingrese el operador (+,-,/,*):");
+	scanf("%c",&op);
+	
+		
+	if(op=='+') printf("suma = %d\n", m+n); // suma
+	else{
+			if(op=='-')
+					printf("Resta = %d\n", m-n); // resta
+			else{
+				if(op=='*')
+					printf("Multiplicación = %d\n", m*n); // multiplicación
+				else{
+					if(n==0) {printf("No definido!\n Vuelva a ingresar el número a dividir:\n"); 	
+						scanf("%d",&n);
+						printf("División = %.2f\n", (float)m/n);}
+					else printf("División = %.2f\n", (float)m/n);}
+			}
+	}	
+}
