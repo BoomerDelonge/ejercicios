0b6d84dfe711a839f309e202b1824ddeae1bfbb6
BoomerDelonge <fiodorste@gmail.com>
2017-03-27 16:10:19 -0500

    Update 123.c

--- 123.c
+++ 123.c
@@ -2,10 +2,12 @@
 char op;
 int x, y, opr;
 void main(void){
-	printf("Ingrese el operador (+,-,/,*):");
-	scanf("%c",&op);
+	
 	printf("Ingrese los números a operar:");
 	scanf("%d %d",&x,&y);
+	getchar();
+	printf("Ingrese el operador (+,-,/,*):");
+	scanf("%c",&op);
 
 	switch(op){
 		case '+' : {opr = x + y ; printf("%d %c %d = %d",x,op,y,opr);break;}
